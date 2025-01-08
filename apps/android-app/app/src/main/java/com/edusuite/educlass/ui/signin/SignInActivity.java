1f288124f62fc2b5e3288ba19a045f3d4d2bcf26
Nguyen Minh Tuan <tnowad@gmail.com>
2025-01-09 03:54:14 +0700

    feat: add sign in view model

--- apps/android-app/app/src/main/java/com/edusuite/educlass/ui/signin/SignInActivity.java
+++ apps/android-app/app/src/main/java/com/edusuite/educlass/ui/signin/SignInActivity.java
@@ -4,23 +4,26 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.edusuite.educlass.R;
+import com.edusuite.educlass.databinding.ActivitySignInBinding;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SignInViewModel signInViewModel = new ViewModelProvider(this).get(SignInViewModel.class);
+        ActivitySignInBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+        binding.setViewModel(signInViewModel);
+        binding.setLifecycleOwner(this);
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
