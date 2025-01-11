9419974c48cb754072d6069641981f7639c98b9d
Nguyen Minh Tuan <tnowad@gmail.com>
2025-01-11 18:27:04 +0700

    feat: add seeds module

---
+++ apps/api/src/seeds/seeds.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: ['src/**/*.entity{.ts,.js}'],
+      logging: true,
+      synchronize: false,
+    }),
+  ],
+})
+export class SeedsModule {}
