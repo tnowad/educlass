21cc04e6854e063d8a86bb33e0a5423f8a9cbcc6
huybang2017 <ndhbang2017@gmail.com>
2025-01-22 20:56:43 +0700

    feat: add mutation getUserByToken

--- apps/api/src/auth/auth.resolver.ts
+++ apps/api/src/auth/auth.resolver.ts
@@ -1,8 +1,11 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { SignInInput } from './dtos/sign-in.input';
 import { SignUpInput } from './dtos/sign-up.input';
 import { AuthService } from './auth.service';
 import { TokensResponse } from './dtos/tokens-response.dto';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from './graphql-auth.guard';
+import { User } from 'src/users/entities/user.entity';
 
 @Resolver()
 export class AuthResolver {
@@ -20,5 +23,16 @@ export class AuthResolver {
     @Args('signUpInput') signUpInput: SignUpInput,
   ): Promise<TokensResponse> {
     return await this.authService.signUp(signUpInput);
+  }
+
+  @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
+  async getUserByToken(@Context('req') req: any): Promise<User> {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      throw new Error('Token không được cung cấp');
+    }
+
+    return await this.authService.validateUser(token);
   }
 }
