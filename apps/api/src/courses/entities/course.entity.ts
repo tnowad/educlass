c2053f07d7a6ba8ecebddfea98b6e9088fec3db9
huybang2017 <ndhbang2017@gmail.com>
2025-02-12 00:16:58 +0700

    feat: add entity course

---
+++ apps/api/src/courses/entities/course.entity.ts
@@ -0,0 +1,48 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType()
+@Entity({ name: 'courses' })
+export class Course {
+  @Field()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field()
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Field({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  section?: string;
+
+  @Field({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  room?: string;
+
+  @Field({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  subject?: string;
+
+  @Field()
+  @Column({ type: 'varchar', length: 255 })
+  code: string;
+
+  @Field()
+  @Column({ name: 'invite_link', type: 'varchar', length: 255 })
+  inviteLink: string;
+
+  @Field()
+  @CreateDateColumn({ name: 'created_at' })
+  createAt: Date;
+
+  @Field()
+  @UpdateDateColumn({ name: 'updated_at' })
+  updateAt: Date;
+}
