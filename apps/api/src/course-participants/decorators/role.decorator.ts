f1c50fa3bda38b149ec4653c99771cd09303ae3e
huybang2017 <ndhbang2017@gmail.com>
2025-02-15 14:21:27 +0700

    fix: remove guard and decorate guard role course participant

--- apps/api/src/course-participants/decorators/role.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { CourseRole } from '../dto/role.enum';
-
-export const CourseRoles = (...roles: CourseRole[]) =>
-  SetMetadata('roles', roles);
