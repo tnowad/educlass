3320b516945e619fc45979244a8d14cd6880b0af
nguyenthuongaz9 <con75580@gmail.com>
2025-02-21 17:14:06 +0700

    chore(posts): update PostsModule to use UsersModule, CoursesModule, and CourseParticipantsModule

--- apps/api/src/posts/posts.module.ts
+++ apps/api/src/posts/posts.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from 'src/posts/entities/post.entity';
 import { PostsService } from './posts.service';
-import { User } from 'src/users/entities/user.entity';
-import { Course } from 'src/courses/entities/course.entity';
 import { PostsResolver } from './posts.resolver';
-import { CourseParticipant } from 'src/course-participants/entities/course-participant.entity';
+import { UsersModule } from 'src/users/users.module';
+import { CourseParticipantsModule } from 'src/course-participants/course-participants.module';
+import { CoursesModule } from 'src/courses/courses.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, User, Course, CourseParticipant])],
+  imports: [
+    TypeOrmModule.forFeature([Post]),
+    UsersModule,
+    CourseParticipantsModule,
+    CoursesModule,
+  ],
   providers: [PostsService, PostsResolver],
   exports: [PostsService],
 })
