fa5645fba09390cafb17bd17ce6a7fc16476f3ee
Nguyen Minh Tuan <tnowad@gmail.com>
2025-03-04 07:30:03 +0700

    feat: add refresh token mutation and auth interceptor

--- apps/android-app/app/src/main/java/com/edusuite/educlass/api/GraphQLModule.java
+++ apps/android-app/app/src/main/java/com/edusuite/educlass/api/GraphQLModule.java
@@ -16,11 +16,18 @@ public class GraphQLModule {
 
     @Provides
     @Singleton
-    public static ApolloClient provideApolloClient(AuthStorage authStorage) {
+    public static ApolloClient provideApolloClient(AuthInterceptor authInterceptor) {
         return new ApolloClient.Builder()
             .serverUrl("http://127.0.0.1:3000/graphql")
-            .addHttpInterceptor(new AuthInterceptor(authStorage))
+            .addHttpInterceptor(authInterceptor)
             .build();
+    }
+
+
+    @Provides
+    @Singleton
+    public static AuthInterceptor provideAuthInterceptor(AuthStorage authStorage) {
+        return new AuthInterceptor(authStorage);
     }
 
 }
